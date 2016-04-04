7a7ceb205c5313db7c621d114ed15a35891dd599
Tuan Nguyen <mtuan93@gmail.com>
2016-04-04 01:12:33 -0700

    Set up database with mongo and dart DTOS

---
+++ tickets/lib/shared/dtos/base_dto.dart
@@ -0,0 +1,6 @@
+part of ticket_schemas;
+
+class BaseDTO {
+  String id;
+  String collection_key;
+}
---
+++ tickets/lib/shared/schemas.dart
@@ -0,0 +1,10 @@
+library ticket_schemas;
+import 'package:dartson/dartson.dart';
+
+// Injectables
+part 'dtos/city_dto.dart';
+part 'dtos/base_dto.dart';
+part 'dtos/purchase_dto.dart';
+part 'dtos/route_dto.dart';
+part 'dtos/time_dto.dart';
+part 'dtos/transaction_dto.dart';
